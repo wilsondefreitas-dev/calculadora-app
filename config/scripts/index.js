5a7f7beb6a72fa951e3d9054cde4ef267f4b31e5
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-10-09 16:03:03 -0300

    operacoes simples

--- config/scripts/index.js
+++ config/scripts/index.js
@@ -1,5 +1,8 @@
-let visorEl;
+let visorEl; 
 let buttonsEls;
+
+let digits = [];
+let operator = '';
 
 //--------------------------------------------------
 
@@ -37,16 +40,76 @@ function handleOnClickOnButtons(event){
   const buttonValue = event.target.innerText; 
   const convertedButtonValue = parseInt(buttonValue);
   const buttonValueIsNumber = (!isNaN(convertedButtonValue));
+  const isDigitsOnVisor = (visorEl.innerText.length > 0);
 
-  if(buttonValueIsNumber === true){
+  if(buttonValue === 'AC') return acOnClick();
+
+  if(buttonValueIsNumber){
 
     writeOnVisor(buttonValue);
 
-  }else{
+  }else if(isDigitsOnVisor){
 
+    saveDigits();
     eraseVisor();
+
+    if(buttonValue === '=' || digits.length === 2){
+
+      getResult();
+
+    }else{
+
+      saveOperator(buttonValue);
+
+    }
     
   }
+
+}
+
+function getResult(){
+
+  let result;
+
+  switch(operator){
+
+    case '+': result = (digits[0] + digits[1]); break;
+    case 'X': result = (digits[0] * digits[1]); break;
+    case '/': result = (digits[0] / digits[1]); break;
+    case '-': result = (digits[0] - digits[1]); break;
+
+  }
+
+  writeOnVisor(result);
+  clearDigits();
+
+}
+
+function saveOperator(_operator){
+
+  operator = _operator;
+
+}
+
+function acOnClick(){
+
+  clearDigits();
+  eraseVisor();
+
+}
+
+function saveDigits(){
+
+  const curDigits = parseInt(visorEl.innerText);
+
+  digits.push(curDigits);
+  console.log(digits);
+
+}
+
+function clearDigits(){
+
+  digits = [];
 
 }
 
